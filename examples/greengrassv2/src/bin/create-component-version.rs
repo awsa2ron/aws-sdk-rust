6b25d8b6f417122d4feede3e9defb76fadd8109b
Aaron Tsui <kaixu@amazon.com>
2022-05-10 20:41:31 +0800

    create-component-version-inline-recipe not works

---
+++ examples/greengrassv2/src/bin/create-component-version.rs
@@ -0,0 +1,80 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_greengrassv2::{Client, Error, Region, PKG_VERSION};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// The AWS Region.
+    #[structopt(short, long)]
+    region: Option<String>,
+
+    /// The Component ARN.
+    #[structopt(long)]
+    arn: String,
+
+    /// The Component ARN.
+    #[structopt(long)]
+    artifact_name: String,
+
+    /// Whether to display additional information.
+    #[structopt(short, long)]
+    verbose: bool,
+}
+
+// Lists your IoT components.
+// snippet-start:[iot.rust.list-component-devices]
+async fn show_components(client: &Client, arn: &str, artifact_name: &str) -> Result<(), Error> {
+    let resp = client.get_component_version_artifact().arn(arn).artifact_name(artifact_name).send().await?;
+
+    println!("components:");
+
+    println!(
+        "    pre-signed url:  {:?}",
+        resp.pre_signed_url().unwrap()
+    );
+
+    println!();
+
+    println!();
+
+    Ok(())
+}
+// snippet-end:[iot.rust.list-component-devices]
+
+/// Lists the name, type, and ARN of your IoT components in the Region.
+///
+/// # Arguments
+///
+/// * `[-r REGION]` - The Region in which the client is created.
+///   If not supplied, uses the value of the **AWS_REGION** environment variable.
+///   If the environment variable is not set, defaults to **us-west-2**.
+/// * `[-v]` - Whether to display information.
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt::init();
+    let Opt { region, arn, artifact_name, verbose } = Opt::from_args();
+
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
+
+    println!();
+    if verbose {
+        println!("IoT client version: {}", PKG_VERSION);
+        println!(
+            "Region:             {}",
+            region_provider.region().await.unwrap().as_ref()
+        );
+        println!();
+    }
+
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
+
+    show_components(&client, &arn, &artifact_name).await
+}
